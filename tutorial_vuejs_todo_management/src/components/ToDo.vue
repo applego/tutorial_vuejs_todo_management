902ccdba1bb5a96990097039359be532c970ba44
applego <applegorillappa@gmail.com>
2020-02-22 14:54:02 +0900

    add ToDo.vue compile success(sass-loader ver7.3.1

---
+++ tutorial_vuejs_todo_management/src/components/ToDo.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Todo Management.</h2>
+    <hr/>
+    <form>
+        <button>ADD TASK</button>
+        <button>DELETE FINISHED TASKS</button>
+        <p>input: <input type="text" ></p>
+        <p>task:</p>
+    </form>
+    <div class="task-list">
+      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-router</label>
+      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vuex</label>
+      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-loader</label>
+      <label class="task-list__item--checked"><input type="checkbox" checked><button>EDIT</button>awesome-vue</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  data () {
+    return {
+      msg: 'Welcome to Your ToDo App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@mixin flex-vender() {
+  display: flex;
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: -ms-flex;
+  display: -o-flex;
+}
+.task-list {
+  @include flex-vender;
+  flex-direction: column;
+  align-items: center;
+  &__item {
+    width: 270px;
+    text-align: left;
+    $element: #{&};
+    &--checked {
+      @extend #{$element};
+      color: #85a6c6;
+    }
+  }
+}
+</style>
--- tutorial_vuejs_todo_management/src/router/index.js
+++ tutorial_vuejs_todo_management/src/router/index.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import ColorSelect from  '@/components/ColorSelect'
+import ColorSelect from '@/components/ColorSelect'
+import ToDo from "@/components/ToDo"; 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
     },
     {
-      path: '/colorselect',
-      name: 'ColorSelect',
+      path: "/colorselect",
+      name: "ColorSelect",
       component: ColorSelect
+    },
+    {
+      path: "/todo",
+      name: "ToDo",
+      component: ToDo
     }
   ]
-})
+});
